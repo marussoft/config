1814020d38ebd853854342bedd97d8cfb987a39f
milinsky <mail@milinsky.ru>
2019-10-17 11:13:10 +1000

    delete Exception

--- src/Config.php
+++ src/Config.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Marussia\Config;
 
 use Marussia\JsonFileLoader\JsonFileLoader;
-use Marussia\Config\Exceptions\DirPathIsAlreadyInstalledException;
 
 class Config
 {
